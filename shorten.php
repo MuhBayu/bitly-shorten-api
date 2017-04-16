aea212efa57a7054328bc27358177991dd388ffc
MuhBayu <bnugraha00@hotmail.com>
2017-04-16 17:52:25 +0700

    fixed

--- shorten.php
+++ shorten.php
@@ -5,13 +5,13 @@ require_once('api-lib/Bitly_API.php');
 
 $long_Url = 'http://bayuu.net';
 
-$_CLASS = new Bitly_API();
-$_CLASS->setAuthID('YOUR_ACCOUNT_ID'); 		// Masukan ID Akun bitly kalian disini
-$_CLASS->setApiKey('YOUR_API_KEY'); 		// Masukan API_KEY kalian disini
-$_CLASS->setSSL(true);						// SSL_VERIFY
-$_CLASS->setDomain(1);						// SHORT DOMAIN
-$_CLASS->setFormat('json');					// OUTPUT FORMAT
+$bitly = new Bitly_API();
+$bitly->setAuthID('YOUR_ACCOUNT_ID'); 	// Masukan ID Akun bitly kalian disini
+$bitly->setApiKey('YOUR_API_KEY'); 		  // Masukan API_KEY kalian disini
+$bitly->setSSL(true);						        // SSL_VERIFY
+$bitly->setDomain(1);						        // SHORT DOMAIN
+$bitly->setFormat('json');					    // OUTPUT FORMAT
 
-echo $_CLASS->shortURL($long_Url);
+echo $bitly->shortURL($long_Url);
 
 ?>
