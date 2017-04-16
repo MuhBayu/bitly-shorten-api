25f1204b6a4bcc1e21e644e50f4e585433d8eaa8
MuhBayu <bnugraha00@hotmail.com>
2017-04-16 17:53:23 +0700

    fixed

--- expand.php
+++ expand.php
@@ -5,12 +5,12 @@ require_once('api-lib/Bitly_API.php');
 
 $short_Url = 'http://bit.ly/2ozR9Hn';
 
-$_CLASS = new Bitly_API();
-$_CLASS->setAuthID('YOUR_ACCOUNT_ID'); 		// Masukan ID Akun bitly kalian disini
-$_CLASS->setApiKey('YOUR_API_KEY'); 		// Masukan API_KEY kalian disini
-$_CLASS->setSSL(true);						// SSL_VERIFY
-$_CLASS->setFormat('json');					// OUTPUT FORMAT
+$bitly = new Bitly_API();
+$bitly->setAuthID('YOUR_ACCOUNT_ID'); 	// Masukan ID Akun bitly kalian disini
+$bitly->setApiKey('YOUR_API_KEY'); 		  // Masukan API_KEY kalian disini
+$bitly->setSSL(true);						        // SSL_VERIFY
+$bitly->setFormat('json');				  	  // OUTPUT FORMAT
 
-echo $_CLASS->expandURL($short_Url);
+echo $bitly->expandURL($short_Url);
 
 ?>
